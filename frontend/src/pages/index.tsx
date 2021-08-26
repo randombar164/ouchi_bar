52975360ea091e6934b25f810d1747059a035252
hide <nkret805worm@gmail.com>
2021-08-26 14:07:07 +0900

    DELETE: useUser削除

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
-import { useGetUser } from "src/utils/hooks/useGetUser";
 
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const uuid = useGetUser();
   return (
     <div className={styles.container}>
       <Head>
