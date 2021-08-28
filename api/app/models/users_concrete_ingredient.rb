e49b808b14b9258ed79bec0bbb6aa9502f8f0042
H0R15H0 <shohori13@gmail.com>
2021-08-28 23:30:35 +0900

    Add: validation for uniqueness

--- api/app/models/users_concrete_ingredient.rb
+++ api/app/models/users_concrete_ingredient.rb
@@ -1,4 +1,9 @@
 class UsersConcreteIngredient < ApplicationRecord
+  validates :user_id, uniqueness: {
+    scope: :concrete_ingredient_id,
+    message: 'Duplicated [user_id, concrete_ingredient_id] detected. It must be unique.'
+  }
+
   belongs_to :user
   belongs_to :concrete_ingredient
 end
--- api/db/migrate/20210825123522_create_users_concrete_ingredients.rb
+++ api/db/migrate/20210825123522_create_users_concrete_ingredients.rb
@@ -5,6 +5,8 @@ class CreateUsersConcreteIngredients < ActiveRecord::Migration[6.0]
       t.references :concrete_ingredient, null: false, foreign_key: true, index: { name: 'users_concrete_ingredients_index_2' }
 
       t.timestamps
+
+      t.index [:user_id, :concrete_ingredient_id], unique: true, name: 'users_concrete_ingredients_index_3'
     end
   end
 end
