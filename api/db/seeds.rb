f0b161bf28c88b0f34f53d5496c33028cc4322e1
H0R15H0 <shohori13@gmail.com>
2021-08-28 22:21:52 +0900

    Add: seeds

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create
+p user.uuid
+# base_ingredientの最初の10件に紐付いているconcrete_ingredientsを酒蔵に登録
+concrete_ingredient_ids = [670, 671, 76, 587, 419, 262, 256, 189, 179, 494, 495, 496, 461]
+concrete_ingredient_ids.each do |ci_id|
+  UsersConcreteIngredient.create(user_id: user.id, concrete_ingredient_id: ci_id)
+end
