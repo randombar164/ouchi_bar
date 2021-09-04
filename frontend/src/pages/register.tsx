3f5f3a6e5696801562a783dcff0ef2e1d97bddf4
morita-koki <morita82007koki@gmail.com>
2021-09-04 20:52:25 +0900

    おおまかな形

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,7 +1,21 @@
+//mochikun用
 import type { VFC } from "react";
+import { IngredientCard, RegisterField } from "src/components/molecules/IngredientCard";
 
 const RegiterPage: VFC = () => {
-  return <div></div>;
+  return (
+  <>
+    <div className=" w-full lg:w-3/4 py-8 px-4 md:px-10 lg:px-16 xl:32 mt-0">
+      <div id="registerPageTitle" className="py-6 font-bold">あなたが持っている材料を登録</div>
+      <RegisterField/>
+      <div id="cardsField" className="py-6 grid grid-cols-1 gap-4">
+        {/* 検索して選択した材料を表示 -> 登録ボタンで一気に登録 */}
+        <IngredientCard/>
+        <IngredientCard/>
+      </div>
+    </div>
+  </>
+  );
 };
 
 export default RegiterPage;
