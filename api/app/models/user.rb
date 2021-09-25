ad54ad352de3f27295c84ea43491e6b6f47cf908
H0R15H0 <shohori13@gmail.com>
2021-09-25 22:36:19 +0900

    Add: users_base_drinks table and add user's cookable cocktails relations.

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
 
   has_many :users_concrete_ingredients, dependent: :destroy
   has_many :concrete_ingredients, through: :users_concrete_ingredients
+  has_many :users_base_drinks, dependent: :destroy
+  has_many :base_drinks, through: :users_base_drinks
+  has_many :cocktails, through: :users_base_drinks, class_name: 'Cocktail'
 
   def set_uuid
     self.uuid = SecureRandom.uuid
@@ -22,13 +25,15 @@ class User < ApplicationRecord
     return drinks
   end
 
-  def cocktails
-    return Cocktail.where(id: self.cookable_base_drinks.pluck(:id))
-    # cocktails = []
-    # base_ingredient_ids = self.base_ingredients.pluck(:id)
-    # Cocktail.with_recipe.includes(:base_drinks_base_ingredients).find_each do |cocktail|
-    #   cocktails.push(cocktail) if cocktail.check_enough_base_ingredients?(base_ingredient_ids)
-    # end
+  # def cocktails
+  #   return Cocktail.where(id: self.cookable_base_drinks.pluck(:id))
+  #   # cocktails = []
+  #   # base_ingredient_ids = self.base_ingredients.pluck(:id)
+  #   # Cocktail.with_recipe.includes(:base_drinks_base_ingredients).find_each do |cocktail|
+  #   #   cocktails.push(cocktail) if cocktail.check_enough_base_ingredients?(base_ingredient_ids)
+  #   # end
+  #   # return cocktails
+  # end
     # return cocktails
   end
 end
