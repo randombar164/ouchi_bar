b3ccb0ac49610d5e9b40cf69fed0a7f44ad72d22
Hide-nu <nkret805worm@gmail.com>
2021-11-03 16:35:59 +0000

    ADD: conditional branch tests

--- frontend/src/pages/_app.page.tsx
+++ frontend/src/pages/_app.page.tsx
@@ -1,15 +1,12 @@
 import "src/styles/globals.css";
 
 import type { AppProps } from "next/app";
-import { Layout } from "src/components/Layout";
 import { ContextProvider } from "src/utils/contexts/provider";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ContextProvider>
-      <Layout>
         <Component {...pageProps} />
-      </Layout>
     </ContextProvider>
   );
 };
---
+++ frontend/src/pages/modal-test/index.page.tsx
@@ -0,0 +1,7 @@
+const Test:React.VFC = (): JSX.Element => {
+    return (
+        <div></div>
+    )
+}
+
+export default Test;
