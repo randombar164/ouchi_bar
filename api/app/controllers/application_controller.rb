f188b73e9f627dc3ac83d2a03cf60a7379c25288
H0R15H0 <shohori13@gmail.com>
2021-11-04 04:54:30 +0900

    Fix: rubocop

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::API
   def response_success(class_name, action_name)
     render status: :ok, json: { status: 200, message: "Success #{class_name} #{action_name}" }
   end
+
   # 404 Not Found
   def response_not_found(class_name = 'Page')
     render status: :not_found, json: { status: 404, message: "#{class_name} Not Found" }
