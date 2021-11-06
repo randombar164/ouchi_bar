c2b2bf66d176bba67db0d97fe4bc6317ca972b90
YutaK1026 <inheritans1026@gmail.com>
2021-11-06 21:03:54 +0900

    UPDATE Scanner

--- frontend/src/pages/scan/index.page.tsx
+++ frontend/src/pages/scan/index.page.tsx
@@ -1,7 +1,21 @@
-import Quagga from '@ericblade/quagga2';
+//import Quagga from '@ericblade/quagga2';
+
 const ScanPage: React.VFC = (): JSX.Element => {
     return (
-    <div>スキャンページです</div>
+    
+        
+    <div>
+        <div className="place-self-start w-auto h-8 bg-gray-800"></div>
+        <div className="place-self-end w-auto h-8 bg-gray-800">
+            <p className = "text-white">
+                バーコード検索
+            </p>
+        </div>
+        <button className="py-2 px-4 text-red-500 bg-white rounded-full border-2 border-white">＜ 登録画面に戻る</button>
+    </div>
+    
     )
-}
+    }
+//ここに、カメラ起動中のUIも書いてOK
+
 export default ScanPage;
