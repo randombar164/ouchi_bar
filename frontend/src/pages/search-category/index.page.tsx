d2f5dd7f7fa7b6336804f4c9a9052fc1c76beb3a
Hide-nu <nkret805worm@gmail.com>
2021-11-11 19:49:54 +0000

    ADD: make category search

--- frontend/src/pages/search-category/index.page.tsx
+++ frontend/src/pages/search-category/index.page.tsx
@@ -1,91 +1,101 @@
-import type { VFC } from "react";
-import { useEffect } from "react";
-import { useState } from "react"
+import React, { useCallback } from "react";
+import { useState } from "react";
 
 import { useGetIngredientsFromCategory } from "../../utils/hooks/useGetIngredientsFromCategory";
-import data from "./category_tree.json"
+import data from "src/static/category_tree.json";
+import { Layout } from "src/components/Layout";
 
-const rootIds: number[] = [];
+type NodeProps = {
+  id: number;
+  name: string;
+  children: any[];
+};
 
-const SearchCategoryPage: VFC = () => {
-    const [parent, setParent] = useState(data.category);
-    const [selecting, setSelecting] = useState(true);
+type CategoryButtonProps = {
+  toNext: (child: NodeProps) => void;
+  child: NodeProps;
+};
 
-    const Title = ({ message }) => {
-        return (
-            <div style={{ background: "#E5E5E5" }} className="pt-5 pb-2 pl-2 text-barOrange-3">{message}</div>
-        )
-    }
-
-    const Back = () => {
-        const back = () => {
-            rootIds.pop();
-            // console.log(rootIds);
-            let parent = data.category;
-            rootIds.forEach((rootId) => {
-                parent = parent.children.find((child) => {
-                    return child.id === rootId;
-                });
-            });
-            setParent(parent);
-        }
-        return (
-            <div style={{ background: "#E5E5E5" }} className="pt-2 pb-5 pl-2 text-barOrange-3">
-                <button onClick={back}>←戻る</button>
-            </div>
-        )
-    }
-
-
-    const ShowChild = ({ child }) => {
-        const { loading, error, response, getIngredientsFromCategory } = useGetIngredientsFromCategory(child.id);
-
-        const toNext = () => {
-            if (child.children.length) { //子要素を持つとき
-                rootIds.push(child.id)
-                setParent(child);
-                return;
-            }
-            else { //子要素を持たないとき
-                console.log("-----CLICKED---------------------");
-                console.log("name: ", child.name);
-                console.log("NodeId: ", child.id);
-
-                getIngredientsFromCategory();//api呼び出し
-                setSelecting(false);
-            }
-        }
-        return (
-            <button onClick={toNext} className="w-full">
-                <div className="py-1 ml-3 text-left">{child.name}</div>
-            </button>
-        )
-    }
-
-    const ShowChildren = ({ _children }) => {
-        return (
-            <div>
-                {_children.map((child: any, idx: number) => { return (<ShowChild key={idx} child={child} />) })}
-            </div>
-        )
-    }
-
+const CategoryButton: React.VFC<CategoryButtonProps> = React.memo(
+  ({ toNext, child }) => {
     return (
-        <div>
-            {selecting && (
-                <div>
-                    <Title message={"カテゴリを選択"} />
-                    <ShowChildren _children={parent.children} />
-                    <Back />
-                </div>)
-            }
-            {!selecting && (
-                <div>
-                    <Title message={"商品を選択"} />
-                </div>)
-            }
+      <button
+        onClick={() => toNext(child)}
+        className="w-full py-1 border-b border-solid border-gray-100"
+      >
+        <div className="py-1 ml-3 text-left">{child.name}</div>
+      </button>
+    );
+  }
+);
+
+const SearchCategoryPage: React.VFC = (): JSX.Element => {
+  const [routeIds, setRouteIds] = useState<number[]>([data.category.id]);
+  const [current, setCurrent] = useState<NodeProps>(data.category);
+  const [hasChild, setHasChild] = useState(true);
+
+  const { loading, error, response, getIngredientsFromCategory, setNodeId } =
+    useGetIngredientsFromCategory();
+
+  const back = useCallback(() => {
+    const parentId = routeIds[routeIds.length - 2];
+    const parent = data.category.children.find((child) => {
+      return child.id === parentId;
+    });
+    if (parent != undefined) {
+      setCurrent(parent);
+    }
+  }, [current]);
+
+  const toNext = useCallback(
+    (child: NodeProps) => {
+      if (child.children.length > 0) {
+        //子要素を持つとき
+        setRouteIds([...routeIds, child.id]);
+        setCurrent(child);
+        return;
+      } else {
+        setHasChild(false);
+        //子要素を持たないとき
+        console.log("-----CLICKED---------------------");
+        console.log("name: ", child.name);
+        console.log("NodeId: ", child.id);
+        setNodeId(child.id);
+        getIngredientsFromCategory(); //api呼び出し
+      }
+    },
+    [current, hasChild, routeIds]
+  );
+  console.log(response);
+  return (
+    <Layout>
+      {hasChild && (
+        <div className="bg-barGray-1 h-screen">
+          <p className="text-barGray-2 pl-2 py-4 font-bold test-sm">
+            カテゴリを選択
+          </p>
+          <div className="h-4/5 overflow-scroll bg-white">
+            {current.children.map((child: NodeProps, i: number) => {
+              return <CategoryButton key={i} toNext={toNext} child={child} />;
+            })}
+          </div>
+          <button
+            className="pl-2 py-4 outline-none text-barGray-2 font-bold text-base"
+            onClick={back}
+          >
+            ←戻る
+          </button>
         </div>
-    )
-}
+      )}
+      {!hasChild && (
+        <div>
+          <p className="text-barGray-2 pl-2 py-4 font-bold test-sm">
+            商品を選択
+          </p>
+        </div>
+      )}
+    </Layout>
+  );
+};
 
 export default SearchCategoryPage;
--- frontend/src/utils/hooks/useGetIngredientsFromCategory.ts
+++ frontend/src/utils/hooks/useGetIngredientsFromCategory.ts
@@ -1,21 +1,22 @@
-import { useEffect } from "react";
 import { useGetApi } from "src/utils/hooks/useApi";
+import { useState } from "react";
 
-export const useGetIngredientsFromCategory = (NodeId: number) => {
+export const useGetIngredientsFromCategory = () => {
+  const [nodeId, setNodeId] = useState(0)
   const { loading, error, response, getFn } = useGetApi(
-    `/queries/search_concrete_ingredient_from_category?category=${NodeId}`
+    `/queries/search_concrete_ingredient_from_category?category=hogehoge`
   );
 
   const getIngredientsFromCategory = () => {
-    if (NodeId !== 0) {
+    if (nodeId !== 0) {
       getFn();
       console.log("---API----------------");
-      console.log(`NodeId=${NodeId}でAPIがたたかれました`);
+      console.log(`NodeId=${nodeId}でAPIがたたかれました`);
       console.log("loading: ", loading);
       console.log("error: ", error);
       console.log("response: ", response);
     }
   };
 
-  return { loading, error, response, getIngredientsFromCategory };
+  return { loading, error, response, getIngredientsFromCategory, setNodeId };
 };
