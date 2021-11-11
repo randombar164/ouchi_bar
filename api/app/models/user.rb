1ecca0f488a320790fbdb94c16360b7fcb1b4043
H0R15H0 <shohori13@gmail.com>
2021-11-11 12:29:14 +0900

    Revert: v1 methods but commented out.

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -33,4 +33,16 @@ class User < ApplicationRecord
       self.users_concrete_ingredients.destroy_by(concrete_ingredient_id: ci_id)
     end
   end
+
+  # ==========
+  # v1 methods
+  # ==========
+  # def update_cookable_cocktails
+  #   cocktail_ids = []
+  #   base_ingredient_ids = self.base_ingredients.pluck(:id)
+  #   V1::Cocktail.includes(:base_drinks_base_ingredients).find_each do |cocktail|
+  #     cocktail_ids.push(cocktail.id) if cocktail.check_enough_base_ingredients?(base_ingredient_ids)
+  #   end
+  #   self.v1_cocktails = V1::Cocktail.where(id: cocktail_ids)
+  # end
 end
