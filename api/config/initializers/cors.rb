5bb6d0e3a1ed782c568ab4d0cabf01a5195ca25c
H0R15H0 <shohori13@gmail.com>
2021-11-13 06:43:59 +0900

    Fix: cors

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     if Rails.env.development?
-      origins 'localhost:3000'
+      origins 'http://localhost:3000'
       resource(
         '*',
         headers: :any,
@@ -17,7 +17,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     end
 
     if Rails.env.production?
-      origins ['www.ouchi-bar.com']
+      origins ['http://www.ouchi-bar.com', 'https://www.ouchi-bar.com']
       resource(
         '*',
         headers: :any,
