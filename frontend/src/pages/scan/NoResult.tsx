2d2b50c1336fe6fc3b511a10812821081043861e
Hide-nu <nkret805worm@gmail.com>
2021-11-12 17:58:03 +0000

    ADD: モーダルの雛形作成

--- frontend/src/pages/scan/NoResult.tsx
+++ frontend/src/pages/scan/NoResult.tsx
@@ -1,17 +1,18 @@
+import { useRouter } from "next/router";
+import { useCallback } from "react";
 import { BgOrangeButton } from "src/components/BgOrangeButton";
 import { Modal } from "src/components/Modal";
 
 export const NoResult = () => {
+  const router = useRouter();
+  const handleClick = useCallback(() => {
+    router.push("/search-category");
+  }, [router]);
   return (
     <Modal isShow={true} onClose={}>
-      <div className="bg-white rounded-xl">
+      <div>
         <p className="text-sm font-bold">材料が見つかりませんでした</p>
-        <BgOrangeButton
-          onClick={() => {
-            return;
-          }}
-          text={"材料名から検索する"}
-        />
+        <BgOrangeButton onClick={handleClick} text={"材料名から検索する"} />
       </div>
     </Modal>
   );
