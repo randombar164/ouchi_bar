f19b68e728a69d5879398ac45a86e302ab45c11c
H0R15H0 <shohori13@gmail.com>
2021-11-12 18:21:47 +0900

    Fix: rubocop

--- api/app/models/category.rb
+++ api/app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
   belongs_to :parent_category, class_name: 'Category', optional: true
-  has_many :concrete_ingredients
+  has_many :concrete_ingredients, dependent: :destroy
 end
