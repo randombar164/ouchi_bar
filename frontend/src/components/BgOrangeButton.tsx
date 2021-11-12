c05db641881b56e195916560b60d4ffb7c86edbd
Hide-nu <nkret805worm@gmail.com>
2021-11-12 22:20:03 +0000

    FIX: 余白変更

--- frontend/src/components/BgOrangeButton.tsx
+++ frontend/src/components/BgOrangeButton.tsx
@@ -3,7 +3,7 @@ export const BgOrangeButton: React.VFC<{ text: string; onClick: () => void }> =
     return (
       <button
         onClick={onClick}
-        className="bg-barOrange-3 rounded-3xl font-bold text-white px-2 py-1 flex justify-center items-center"
+        className="bg-barOrange-2 mx-auto rounded-3xl font-bold text-white px-5 py-2 flex justify-center items-center"
       >
         {text}
       </button>
