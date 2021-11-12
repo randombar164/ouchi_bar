0a34c902cee8f8c3ad3a7e33d36dc27f8a32c213
Hide-nu <nkret805worm@gmail.com>
2021-11-12 22:20:33 +0000

    FIX: スタイル修正

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -14,34 +14,26 @@ export const Modal: React.VFC<Props> = ({
   className,
 }) => {
   return (
-    <Transition appear show={isShow} as={Fragment}>
+    <Transition appear show={isShow} as="div">
       <Dialog
         as="div"
-        //   className="fixed inset-0 z-10 overflow-y-auto"
+        className="overflow-y-auto fixed inset-0 z-50"
         onClose={onClose}
       >
-        <div className="px-4 min-h-screen text-center">
+        <div className="py-7 h-screen">
           <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
+            // as="div"
+            enter="ease-out duration-500"
             enterFrom="opacity-0"
             enterTo="opacity-100"
             leave="ease-in duration-200"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Dialog.Overlay className="fixed inset-0" />
+            <Dialog.Overlay className="fixed inset-0 bg-black opacity-20" />
           </Transition.Child>
-
-          {/* This element is to trick the browser into centering the modal contents. */}
-          <span
-            className={`inline-block h-screen align-middle ${className}`}
-            aria-hidden="true"
-          >
-            &#8203;
-          </span>
           <Transition.Child
-            as={Fragment}
+            as="div"
             enter="ease-out duration-300"
             enterFrom="opacity-0 scale-95"
             enterTo="opacity-100 scale-100"
@@ -49,7 +41,11 @@ export const Modal: React.VFC<Props> = ({
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-            <div className="bg-white rounded-xl">{children}</div>
+            <div
+              className={`mx-auto w-full max-w-xs md:max-w-xl bg-white rounded-3xl transition-all transform ${className}`}
+            >
+              {children}
+            </div>
           </Transition.Child>
         </div>
       </Dialog>
