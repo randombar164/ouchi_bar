6bb111d55b4fb720a1ab13c44932914a3c0a6a29
H0R15H0 <shohori13@gmail.com>
2021-11-13 11:46:34 +0900

    Fix: SearchConcreteIngredientFromCategory to find category by amazon_browse_node_id

--- api/app/controllers/queries/search_concrete_ingredient_from_category_controller.rb
+++ api/app/controllers/queries/search_concrete_ingredient_from_category_controller.rb
@@ -1,6 +1,7 @@
 class Queries::SearchConcreteIngredientFromCategoryController < ApplicationController
   def execute
-    category = Category.find(params[:id])
+    category = Category.find_by(amazon_browse_node_id: params[:id])
+    response_not_found('category') and return if category.nil?
     concrete_ingredients = category.concrete_ingredients
     render json: { concrete_ingredients: concrete_ingredients }
   end
