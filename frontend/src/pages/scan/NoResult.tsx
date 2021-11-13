e1c8b1415fab34d09bec5a7601bcbfc06601deb0
Hide-nu <nkret805worm@gmail.com>
2021-11-13 01:48:36 +0000

    UPDATE: 検索から登録のAPIを連携(動作未確認)

--- frontend/src/pages/scan/NoResult.tsx
+++ frontend/src/pages/scan/NoResult.tsx
@@ -2,16 +2,17 @@ import { useRouter } from "next/router";
 import { useCallback } from "react";
 import { BgOrangeButton } from "src/components/BgOrangeButton";
 import { Modal } from "src/components/Modal";
+import type { ModalProps } from "src/utils/types/type";
 
-export const NoResult = () => {
+export const NoResult: React.VFC<ModalProps> = (prop): JSX.Element => {
   const router = useRouter();
   const handleClick = useCallback(() => {
     router.push("/search-category");
   }, [router]);
   return (
-    <Modal isShow={true} onClose={}>
-      <div>
-        <p className="text-sm font-bold">材料が見つかりませんでした</p>
+    <Modal isShow={prop.isShow} onClose={prop.onClose} className="mt-48">
+      <div className="text-center py-6">
+        <p className="text-sm font-bold mb-4">材料が見つかりませんでした</p>
         <BgOrangeButton onClick={handleClick} text={"材料名から検索する"} />
       </div>
     </Modal>
--- frontend/src/pages/scan/index.page.tsx
+++ frontend/src/pages/scan/index.page.tsx
@@ -1,21 +1,66 @@
-import React, { useState, useRef, useLayoutEffect } from "react";
-import Quagga, {
-  QuaggaJSResultObject,
-  QuaggaJSResultCallbackFunction,
-} from "@ericblade/quagga2";
+import React, { useState, useRef, useCallback, useEffect } from "react";
 import { Scanner } from "./Scanner";
+import { NoResult } from "./NoResult";
+import { useSearchByCode } from "src/utils/hooks/useSearchByCode";
+import { SingleResult } from "./SingleResult";
+import { MultiResults } from "./MultiResults";
+import Link from "next/link";
 
 const ScanPage: React.VFC = (): JSX.Element => {
-  const [results, setResults] = useState<QuaggaJSResultObject[]>([]);
+  const [results, setResults] = useState<string[]>([]);
+  const [isShow, setIsShow] = useState(false);
   const scannerRef = useRef<HTMLDivElement | null>(null);
+
+  const { loading, response, searchByCode } = useSearchByCode();
+
+  console.log(results);
+
+  const handleClose = useCallback(() => {
+    setIsShow(false);
+  }, [isShow]);
+
+  useEffect(() => {
+    if (results.length < 1) {
+      return;
+    }
+    if (loading) {
+      return;
+    }
+    searchByCode(results[0]);
+    setIsShow(true);
+  }, [results]);
+
   return (
     <>
+      <SingleResult
+        ingredient={response?.concreteIngredient}
+        isShow={isShow && !!response?.foundFromDatabase}
+        onClose={handleClose}
+      />
+      <MultiResults
+        ingredients={response?.result}
+        isShow={
+          isShow && !response?.foundFromDatabase && response?.result?.length > 0
+        }
+        onClose={handleClose}
+        code={results[0]}
+      />
+      <NoResult
+        isShow={
+          isShow &&
+          !response?.foundFromDatabase &&
+          response?.result?.length == 0
+        }
+        onClose={handleClose}
+      />
       <div className="w-full py-4 bg-gray-800 fixed top-0 z-30">
-        <button className="block text-sm ml-3 font-bold py-2 px-4 text-red-500 bg-white rounded-full border-2 border-white">
-          &lt; 登録画面に戻る
-        </button>
+        <Link href="/register">
+          <div className="inline-block text-sm ml-3 font-bold py-2 px-4 text-red-500 bg-white rounded-full border-2 border-white">
+            &lt; 登録画面に戻る
+          </div>
+        </Link>
       </div>
-      <div ref={scannerRef} className="relative w-full h-screen">
+      <div ref={scannerRef} className="relative w-full h-screen mt-16">
         <canvas
           className="drawingBuffer absolute top-0"
           width="375"
@@ -23,9 +68,7 @@ const ScanPage: React.VFC = (): JSX.Element => {
         />
         <Scanner
           scannerRef={scannerRef}
-          onDetected={(result: QuaggaJSResultObject) =>
-            setResults([...results, result])
-          }
+          onDetected={(result: string) => setResults([...results, result])}
         />
       </div>
       <div className="z-30 w-full h-12 bg-gray-800 fixed bottom-0" />
