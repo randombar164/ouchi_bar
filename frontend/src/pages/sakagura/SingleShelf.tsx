87def1583e87ea1b712df5bc0e61817c791b7d17
Hide-nu <nkret805worm@gmail.com>
2021-11-13 01:57:57 +0000

    fix: リクエストボディを別関数の引数として設置

--- frontend/src/pages/sakagura/SingleShelf.tsx
+++ frontend/src/pages/sakagura/SingleShelf.tsx
@@ -14,7 +14,7 @@ export const SingleShelf: VFC<Props> = ({ items }) => {
             <div key={i} className="w-[40px]">
               <Image
                 src={item.img_src}
-                alt="適当なaltタグを設置してください"
+                alt="お酒の画像"
                 width={40}
                 height={110}
                 layout={"responsive"}
