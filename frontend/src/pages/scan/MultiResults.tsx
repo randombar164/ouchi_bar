97c334bf1178d02cf8c45ecaad20aae90ac60970
Hide-nu <nkret805worm@gmail.com>
2021-11-13 01:48:36 +0000

    UPDATE: 検索から登録のAPIを連携(動作未確認)

--- frontend/src/pages/scan/MultiResults.tsx
+++ frontend/src/pages/scan/MultiResults.tsx
@@ -1,5 +1,64 @@
 import { Modal } from "src/components/Modal";
+import type { ModalProps } from "src/utils/types/type";
+import { ResultCard } from "src/pages/scan/ResultCard";
+import { Context } from "src/utils/contexts/provider";
+import { useCallback, useContext, useEffect } from "react";
+import { useRouter } from "next/router";
+import { useRegisterCode } from "src/utils/hooks/useRegisterCode";
 
-export const MultiResults: React.VFC = (): JSX.Element => {
-  return <div></div>;
+type Props = {
+  ingredients: any[];
+  code: string;
+} & ModalProps;
+export const MultiResults: React.VFC<Props> = (prop): JSX.Element => {
+  const { concreteIngredients, setConcreteIngredients } = useContext(Context);
+  const router = useRouter();
+
+  const { loading, response, registerCode } = useRegisterCode();
+  const handleClick = useCallback(
+    (asin: string) => {
+      registerCode({
+        jan_code: prop.code,
+        asin: asin,
+      });
+    },
+    [prop]
+  );
+
+  useEffect(() => {
+    if (!response) {
+      return;
+    }
+
+    setConcreteIngredients([
+      ...concreteIngredients,
+      response.concreteIngredients,
+    ]);
+    router.push("/register");
+  }, [response]);
+  return (
+    <Modal
+      isShow={prop.isShow}
+      onClose={prop.onClose}
+      className="overflow-scroll h-5/6 my-auto max-h-[700px]"
+    >
+      <div className="py-8 px-6">
+        <p className="text-barGray-3 font-bold text-sm">
+          どれか一つをタップして追加してください
+        </p>
+        {prop.ingredients &&
+          prop.ingredients.map((ingredient, i) => {
+            return (
+              <div className="my-4" key={i}>
+                <ResultCard
+                  imgSrc={ingredient.images.primary.medium.url}
+                  name={ingredient.itemInfo.title.displayValue}
+                  onClick={() => handleClick(ingredient.asin)}
+                />
+              </div>
+            );
+          })}
+      </div>
+    </Modal>
+  );
 };
--- frontend/src/pages/scan/SingleResult.tsx
+++ frontend/src/pages/scan/SingleResult.tsx
@@ -1,5 +1,32 @@
 import { Modal } from "src/components/Modal";
+import type { ModalProps, IngredientCardProps } from "src/utils/types/type";
+import { ResultCard } from "src/pages/scan/ResultCard";
+import { Context } from "src/utils/contexts/provider";
+import { useCallback, useContext } from "react";
+import { useRouter } from "next/router";
 
-export const SingleResult: React.VFC = (): JSX.Element => {
-  return <div></div>;
+type Props = {
+  ingredient: any;
+} & ModalProps;
+
+export const SingleResult: React.VFC<Props> = (prop): JSX.Element => {
+  const { concreteIngredients, setConcreteIngredients } = useContext(Context);
+  const router = useRouter();
+  const handleClick = useCallback(() => {
+    setConcreteIngredients([...concreteIngredients, prop.ingredient]);
+    router.push("/register");
+  }, [prop, setConcreteIngredients, router]);
+  return (
+    <Modal isShow={prop.isShow} onClose={prop.onClose} className="mt-48">
+      <div className="py-8 px-6">
+        {prop.ingredient && (
+          <ResultCard
+            imgSrc={prop.ingredient.imgSrc}
+            name={prop.ingredient.name}
+            onClick={handleClick}
+          />
+        )}
+      </div>
+    </Modal>
+  );
 };
