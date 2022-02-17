fdfcd4b6ab04b695c98950beaf6efbdda2fa77a5
akaishikiso <lycopene109@outlook.jp>
2022-02-17 18:12:05 +0900

    drinkMethodにブレンドを追加

--- frontend/src/components/CocktailImg.tsx
+++ frontend/src/components/CocktailImg.tsx
@@ -7,10 +7,11 @@ const CocktailImg: React.VFC<CocktailImgProps> = ({ recipe }) => {
     <div className="">
       if ({recipe.drinkMethod}="Build")
       {<img src="/build.png" width={49} height={177} alt="ビルド" />}
-      else if ({recipe.drinkMethod}="Stir")
-      {<img src="/stir.png" width={49} height={177} alt="ステア" />}
-      else
-      {<img src="/shake.png" width={49} height={177} alt="シェイク" />}
+      else if ({recipe.drinkMethod}="Shake")
+      {<img src="/stir.png" width={49} height={177} alt="シェイク" />}
+      else if ({recipe.drinkMethod}="Blend")
+      {<img src="/blend.png" width={49} height={177} alt="ブレンド" />}else
+      {<img src="/shake.png" width={49} height={177} alt="ステア" />}
     </div>
   );
 };
