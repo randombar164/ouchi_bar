2451ce00f0a8bba133e2db83427c66d66d9dc99e
akaishikiso <lycopene109@outlook.jp>
2022-02-19 11:17:31 +0900

    APIをたたく回数を減らすよう変更しました

--- frontend/src/components/CocktailImg.tsx
+++ frontend/src/components/CocktailImg.tsx
@@ -9,8 +9,10 @@ const CocktailImg: React.VFC<CocktailImgProps> = ({ recipe }) => {
       {<img src="/build.png" width={49} height={177} alt="ビルド" />}
       else if ({recipe.drinkMethod}="Stir")
       {<img src="/stir.png" width={49} height={177} alt="ステア" />}
-      else
+      else if ({recipe.drinkMethod}="Shake")
       {<img src="/shake.png" width={49} height={177} alt="シェイク" />}
+      else
+      {<img src="/blend.png" width={49} height={177} alt="ブレンド" />}
     </div>
   );
 };
