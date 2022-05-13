ef30ea6997489acd14366b6c85983b1360e7dd32
Hide-nu <nkret805worm@gmail.com>
2022-05-14 02:49:21 +0900

    写真で認識する方式にする予定

--- frontend/src/pages/scan/Scanner.tsx
+++ frontend/src/pages/scan/Scanner.tsx
@@ -13,66 +13,120 @@ type Props = {
   setError: (error: Exception) => void;
 };
 
+const timeout = 1000;
+const scale = 0.5;
+
+let videoStream: MediaStream;
+
 export const Scanner: React.VFC<Props> = ({ isScan, setCode, setError }) => {
-  const codeReader = new BrowserMultiFormatReader();
+  const barcodeReader = new BrowserMultiFormatReader();
+
   const formats = [
     BarcodeFormat.DATA_MATRIX,
     BarcodeFormat.CODE_39,
     BarcodeFormat.CODE_128,
   ];
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const hints = new Map();
 
+  const constrains = { video: true };
   useEffect(() => {
-    hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [hints]);
+    const video = document.querySelector("#scanner-video") as HTMLVideoElement;
+    const canvas = document.querySelector(
+      "#scanner-canvas"
+    ) as HTMLCanvasElement;
+    const img = document.querySelector("#scanner-image") as HTMLImageElement;
+    const frame = document.querySelector("#scanner-frame") as HTMLDivElement;
+    navigator.mediaDevices.getUserMedia(constrains).then((stream) => {
+      console.log(video);
+      videoStream = stream;
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
 
-  useEffect(() => {
-    if (isScan) {
-      captureStart((result, error) => {
-        if (error) {
-          setError(error);
-        }
-        setCode(result.getText());
-      });
-    } else {
-      captureStop();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isScan]);
+      frame.style.width = video.clientWidth * scale + "px";
+      frame.style.height = video.clientHeight * scale + "px";
+      frame.style.left = (window.innerWidth - video.clientWidth) / 2 + "px";
+      frame.style.top = (window.innerHeight - video.clientHeight) / 2 + "px";
+      console.log(video.clientHeight);
 
-  const captureStart = (
-    callback: (result: Result, error: Exception | undefined) => void,
-    stopOnCapture = true
-  ) => {
-    codeReader.decodeFromVideoDevice("", videoRef.current, (result, error) => {
-      if (result) {
-        // setScanResult(result);
-        if (stopOnCapture === true) {
-          captureStop();
-        }
-        callback(result, error);
-      }
-      if (error && !(error instanceof NotFoundException)) {
-        console.error(error);
-      }
+      scanFrame(canvas, video, img);
+      // video?.addEventListener("play", () => {});
     });
+  }, []);
+
+  const scanFrame = (
+    canvas: HTMLCanvasElement,
+    video: HTMLVideoElement,
+    img: HTMLImageElement
+  ) => {
+    if (videoStream) {
+      canvas.getContext("2d")?.drawImage(
+        video,
+
+        // source x, y, w, h
+        (video.videoWidth * (1 - scale)) / 2,
+        (video.videoHeight * (1 - scale)) / 2,
+        video.videoWidth * scale,
+        video.videoHeight * scale,
+
+        // dest x, y, w, h
+        0,
+        0,
+        canvas.width,
+        canvas.height
+      );
+
+      canvas.toBlob((blob) => {
+        const url = URL.createObjectURL(blob);
+
+        img.onload = async () => {
+          barcodeReader
+            .decodeFromImage(undefined, url)
+            .then(found)
+            .catch(notFound)
+            .finally(() => {
+              releaseMemory(img);
+            });
+          img.onload = null;
+          setTimeout(scanFrame, timeout);
+        };
+        img.src = url;
+      });
+    }
   };
-  const captureStop = () => {
-    // setIsRunning(false);
-    codeReader.reset();
+
+  const found = (result: Result) => {
+    setCode(result.getText());
+  };
+
+  const notFound = (err: Exception) => {
+    setError(err);
+  };
+
+  const releaseMemory = (img: HTMLImageElement) => {
+    URL.revokeObjectURL(img.src);
+    img.src = "";
   };
 
   return (
     <div className="relative h-screen">
-      {/*eslint-disable-next-line jsx-a11y/media-has-caption*/}
-      <video
-        id="video"
-        className="object-cover absolute h-screen"
-        ref={videoRef}
-        style={{ border: "1px solid gray" }}
-      ></video>
+      <div className="absolute inset-0 z-10 m-auto bg-gray-700">
+        {/*eslint-disable-next-line jsx-a11y/media-has-caption*/}
+        <video
+          id="scanner-video"
+          className="absolute inset-0 z-20 m-auto w-auto h-screen"
+          style={{ border: "1px solid gray" }}
+        ></video>
+      </div>
+      <div
+        id="scanner-frame"
+        className="absolute z-30 m-auto border-2 border-barOrange-3 border-solid shadow-md drop-shadow-md"
+      ></div>
+      <canvas id="scanner-canvas" className="z-40 m-auto w-full"></canvas>
+      <img
+        id="scanner-image"
+        src=""
+        alt="スキャナーイメージ"
+        className="z-10 m-auto w-full"
+      />
     </div>
   );
 };
