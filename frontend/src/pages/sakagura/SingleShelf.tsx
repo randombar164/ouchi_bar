833690a4d522aa104a4849f5ee4f5c5790acfb99
YutaK1026 <inheritans1026@gmail.com>
2022-06-16 16:48:50 +0900

    コメントアウト削除

--- frontend/src/pages/sakagura/SingleShelf.tsx
+++ frontend/src/pages/sakagura/SingleShelf.tsx
@@ -17,15 +17,6 @@ export const SingleShelf: VFC<Props> = ({ items }) => {
           );
         })}
       </Box>
-      {/* <div className="flex justify-around items-end w-full h-[150px]">
-        {items.map((item, i) => {
-          return (
-            <div key={i} className="w-[40px]">
-              <img src={item.imgSrc} alt="お酒の画像" width={40} height={110} />
-            </div>
-          );
-        })}
-      </div> */}
       <div className={`${styles.shelf}`}></div>
     </div>
   );
