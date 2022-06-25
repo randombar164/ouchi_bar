df557cd35ce2d4731d10f63294494bc580fb8633
morita-koki <morita82007koki@gmail.com>
2022-06-25 18:30:36 +0900

    途中経過

--- frontend/src/pages/search-category/index.page.tsx
+++ frontend/src/pages/search-category/index.page.tsx
@@ -11,8 +11,10 @@ import { useGetIngredientsFromCategory } from '../../utils/hooks/useGetIngredien
 
 /* MUI */
 // import { Container } from "@mui/material";
-import { ListItemText } from "@mui/material"
 import { Box } from "@mui/material";
+import { List } from "@mui/material";
+// import { ListItem } from "@mui/material";
+import { Button } from "@mui/material";
 
 type NodeProps = {
   id: number;
@@ -98,30 +100,46 @@ const SearchCategoryPage: React.VFC = (): JSX.Element => {
       <Box sx={{ background: "#EEEEEE" }}>
         {hasChild && (
           <Box>
-            <p className="py-4 pl-2 text-sm font-bold text-barGray-2">
-              カテゴリを選択
-            </p>
-            <div className="overflow-scroll h-auto max-h-[70vh] bg-white">
+            <Box sx={{ 
+              fontWeight: "bold", 
+              fontSize: "14px", 
+              color: "#A7B6C8", 
+              py: 2, 
+              px: 1 
+              }}>カテゴリを選択</Box>
+            <List sx={{
+              width: "100%",
+              overflowY: "scroll",
+              backgroundColor: "white",
+            }}>
               {current.children.map((child: NodeProps, i: number) => {
-                return <CategoryButton key={i} toNext={toNext} child={child} />;
+                return <CategoryButton key={i} toNext={toNext} child={child} />; // FIXME: CartegoryButtonでtailWindを使っている
               })}
-            </div>
+            </List>
             {routeIds.length >= 1 && (
-              <button
-                className="py-4 pl-2 text-base font-bold text-barGray-2 outline-none"
-                onClick={back}
-              >
-                ←戻る
-              </button>
+              <Button
+                sx={{
+                  py: 2,
+                  px: 1,
+                  fontSize: "16px",
+                  fontWeight: "bold",
+                  color: "#A7B6C8",
+                  outline: "2px solid transparent",
+                  outlineOffset: "2px"
+                }}>←戻る</Button>
             )}
           </Box>
         )}
 
         {!hasChild && response?.concreteIngredients?.length > 0 && (
-          <div className="px-3">
-            <p className="py-4 font-bold text-barGray-2">
-              商品を選択(1つ選んでタップしてください)
-            </p>
+          <Box sx={{ px: 2 }}>
+            <Box sx={{ 
+              fontWeight: "bold", 
+              fontSize: "14px", 
+              color: "#A7B6C8", 
+              py: 2, 
+              px: 1 
+              }}>商品を選択（１つ選んでタップしてください）</Box>
             {response?.concreteIngredients?.map((ingredient: any, i: any) => {
               return (
                 <div className="my-3" key={i}>
@@ -136,21 +154,29 @@ const SearchCategoryPage: React.VFC = (): JSX.Element => {
                 </div>
               );
             })}
-          </div>
+          </Box>
         )}
         {!hasChild && response?.concreteIngredients?.length == 0 && (
-          <div className="py-10">
-            <p className="font-bold text-center text-barGray-3">
-              検索結果が0件でした
-            </p>
-            <button
-              onClick={onReload}
-              className="py-4 pl-2 text-base font-bold text-barGray-2 outline-none"
-            >
-              ←戻る
-            </button>
-          </div>
-        )}
+          <Box sx={{ py: 10 }}>
+            <Box sx={{
+              fontWeight: "bold",
+              color: "#505050",
+              textAlign: "center",
+            }}>検索結果が０件でした</Box>
+            <Button
+              variant="text"
+              onClick={() => {onReload()}}
+              sx={{
+                py: 2,
+                px: 1,
+                fontSize: "16px",
+                lineHeight: "24px",
+                fontWeight: "bold",
+                color: "#A7B6C8",
+                outline: "2px solid transparent",
+                outlineOffset: "2px"
+              }}>←戻る</Button>
+          </Box>)}
       </Box>
     </Layout>
   );
