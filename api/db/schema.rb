aba9db2fb9b6839f57dc76dd505db0bbc6d2a2d5
H0R15H0 <shohori13@gmail.com>
2022-06-30 15:28:43 +0900

    unitにis_display_before_amountを追加するmigrationを追加

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_27_105416) do
+ActiveRecord::Schema.define(version: 2022_06_30_060644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -271,6 +271,7 @@ ActiveRecord::Schema.define(version: 2022_06_27_105416) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_display_before_amount", default: false, null: false
   end
 
   create_table "v3_users", force: :cascade do |t|
