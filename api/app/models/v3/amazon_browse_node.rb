064b59b9369942e041cfc5364578c47257e35164
H0R15H0 <shohori13@gmail.com>
2022-06-30 16:59:43 +0900

    テーブル名を複数形に修正

--- api/app/models/v3/amazon_browse_node.rb
+++ api/app/models/v3/amazon_browse_node.rb
@@ -1,5 +1,5 @@
 class V3::AmazonBrowseNode < ApplicationRecord
-  self.table_name = 'v3_amazon_browse_node'
+  self.table_name = 'v3_amazon_browse_nodes'
 
   belongs_to :parent_category, class_name: 'V3::Category', optional: true
   has_many :concrete_ingredients, dependent: :destroy
