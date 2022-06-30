3b910f99c0c7c8a3956e95fa94d0bc4fa2eca32c
H0R15H0 <shohori13@gmail.com>
2022-06-30 17:15:59 +0900

    明治テーブル名を複数形に修正。

--- api/app/models/v3/ingredients_handling_store.rb
+++ api/app/models/v3/ingredients_handling_store.rb
@@ -1,5 +1,5 @@
 class V3::IngredientsHandlingStore < ApplicationRecord
-  self.table_name = 'v3_ingredient_handling_stores'
+  self.table_name = 'v3_ingredients_handling_stores'
 
   belongs_to :concrete_ingredient
   belongs_to :handling_store
