00df11e84a664629af80151d784d8f0382b009b4
H0R15H0 <shohori13@gmail.com>
2022-06-30 15:27:32 +0900

    amazon_brose_nodeのtable_nameを修正

--- api/app/models/v3/amazon_browse_node.rb
+++ api/app/models/v3/amazon_browse_node.rb
@@ -1,5 +1,5 @@
 class V3::AmazonBrowseNode < ApplicationRecord
-  self.table_name = 'v3_categories'
+  self.table_name = 'v3_amazon_browse_node'
 
   belongs_to :parent_category, class_name: 'V3::Category', optional: true
   has_many :concrete_ingredients, dependent: :destroy
