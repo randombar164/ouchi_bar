879d2b0d9c9675a00e6d59bf6184365e7ae751ae
H0R15H0 <shohori13@gmail.com>
2022-06-30 18:38:49 +0900

    commands/register_ingredient_by_amazon_productのv3移行DONE テストも追加

--- api/app/models/v3/amazon_browse_node.rb
+++ api/app/models/v3/amazon_browse_node.rb
@@ -1,6 +1,5 @@
 class V3::AmazonBrowseNode < ApplicationRecord
   self.table_name = 'v3_amazon_browse_nodes'
 
-  belongs_to :parent_category, class_name: 'V3::Category', optional: true
-  has_many :concrete_ingredients, dependent: :destroy
+  has_one :category
 end
