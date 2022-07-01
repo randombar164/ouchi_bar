a04e62c610d5e5858d5df588d77380b710f7ff4c
Hide-nu <nkret805worm@gmail.com>
2022-07-01 15:51:35 +0900

    結果のカードをmuiに移行

--- frontend/src/pages/scan/ResultCard.tsx
+++ frontend/src/pages/scan/ResultCard.tsx
@@ -1,3 +1,7 @@
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 import type { IngredientCardProps } from "src/utils/types/type";
 
 type Props = {
@@ -6,12 +10,21 @@ type Props = {
 
 export const ResultCard: React.VFC<Props> = (prop) => {
   return (
-    <button
-      onClick={prop.onClick}
-      className="flex overflow-hidden gap-3 justify-center items-center py-3 w-full h-24 rounded-xl border border-gray-300 border-solid"
-    >
-      <img src={prop.imgSrc} alt={`${prop.name}の画像`} className="h-20" />
-      <p className="overflow-hidden w-40 text-sm font-bold">{prop.name}</p>
-    </button>
+    <Card>
+      <CardActionArea
+        sx={{ display: "flex", height: 80 }}
+        onClick={prop.onClick}
+      >
+        <CardMedia
+          component="img"
+          sx={{ width: 160, objectFit: "contain" }}
+          image={prop.imgSrc}
+          alt={`${prop.name}の画像`}
+        />
+        <Typography variant="body1" sx={{ overflow: "hidden" }}>
+          {prop.name}
+        </Typography>
+      </CardActionArea>
+    </Card>
   );
 };
