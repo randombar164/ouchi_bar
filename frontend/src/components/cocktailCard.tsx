081ab00f5eb864e9e58aefbe6d58145b9d8c5741
YAMADA Riko <lycopene109@outlook.jp>
2022-07-02 22:12:50 +0900

    gridにした

--- frontend/src/components/cocktailCard.tsx
+++ frontend/src/components/cocktailCard.tsx
@@ -1,3 +1,4 @@
+import { Card, Grid } from '@mui/material';
 import Link from 'next/link';
 import type { VFC } from 'react';
 
@@ -25,16 +26,20 @@ export const CocktailCard: VFC<CocktailProps> = ({ cocktails }) => {
       {cocktails.map((cocktail, recipe) => {
         return (
           <Link href={`/cocktail-recipe/${cocktail.id}`} key={cocktail.id}>
-            <div className="box-content flex p-2 w-auto h-20 border-b-2 border-barGray-1">
-              <CocktailImg recipe={recipe} />
-              <div className="m-2 w-56">
+            <Card variant="outlined">
+            <Grid container spacing={1}>
+              <Grid item xs={3}>
+                <CocktailImg recipe={recipe} />
+              </Grid>
+              <Grid item xs={9}>
                 <h1 className="py-2 font-bold text-left">{cocktail.name}</h1>
                 <h2 className="text-xs text-left text-blue-500">
                   {'すぐ作れる！'}
                 </h2>
-              </div>
-              <div className="px-2 my-auto h-16 text-xl">{'>'}</div>
-            </div>
+              </Grid>
+            </Grid>
+            </Card>
+           
           </Link>
         );
       })}
