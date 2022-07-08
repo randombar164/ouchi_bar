05cd3843a15368059e4148358287097cd556bea0
H0R15H0 <shohori13@gmail.com>
2022-07-08 17:29:02 +0900

    show_cocktailでCocktailが見つからなかった時のエラーハンドリングがなくてのテストが落ちていたので修正

--- api/spec/requests/v3/queries/show_cocktail_spec.rb
+++ api/spec/requests/v3/queries/show_cocktail_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'V3::Queries::ShowCocktail', type: :request do
-  let(:cocktail) { Cocktail.first }
+  let!(:drink_method) { V3::DrinkMethod.create!(name: "drink_method_1") }
+  let!(:glass_type) { V3::GlassType.create!(name: "glass_type_1") }
+  let!(:cocktail) { V3::Cocktail.create!(name: "cocktail_1", drink_method_id: drink_method.id, glass_type_id: glass_type.id) }
 
   # カクテルのデータを入れると動きます。
   describe 'GET /v3/queries/show_cocktail' do
     it 'responds successfully returns a 200 response' do
       get v3_queries_show_cocktail_path, params: { id: cocktail.id }
       expect(response).to be_successful
-      expect(JSON.parse(response.body)['cocktail']).not_to eq nil
+      expect(JSON.parse(response.body)['cocktail']['id']).to eq cocktail.id
+      expect(JSON.parse(response.body)['cocktail']['name']).to eq cocktail.name
     end
 
     it 'responds unsuccessfully returns a 404 response' do
