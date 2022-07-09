b25587c383f8e0ca45e1f539e60ed53c77a456d3
morita-koki <morita82007koki@gmail.com>
2022-07-09 22:54:10 +0900

    /ingredient作成

--- frontend/src/pages/ingredient/index.page.tsx
+++ frontend/src/pages/ingredient/index.page.tsx
@@ -1,13 +1,49 @@
+import { useRouter } from "next/router";
+
 /* original component */
 import { Layout } from "src/components/Layout";
+import { IngredientCard } from "src/components/IngredientCard";
+
+/* type */
+import type { Ingredient } from "src/utils/types/type";
 
 /* MUI Component */
 
 const IngredientPage: React.VFC = (): JSX.Element => {
+  const sample_ingredients: Ingredient[] = [
+    {
+      img: "/whisky.png",
+      name: "whisky",
+      content: "whisky content",
+    },
+    {
+      img: "/whisky.png",
+      name: "whisky",
+      content: "whisky content",
+    },
+    {
+      img: "/whisky.png",
+      name: "whisky",
+      content: "whisky content",
+    },
+  ];
+
+  const router = useRouter();
+
   return (
     <Layout>
-      <div>ingredient index page</div>
-      <div>材料一覧を表示する</div>
+      {sample_ingredients?.map((ingredient) => {
+        return (
+          <IngredientCard //TODO: IngredientCardの修正
+            canDelete={false}
+            imgSrc={ingredient.img}
+            name={ingredient.name}
+            onClick={() => {
+              router.push("/ingredient/show");
+            }}
+          />
+        );
+      })}
     </Layout>
   );
 };
