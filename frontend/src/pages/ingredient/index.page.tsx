c35daead6e6ae158bc0b7b4b4c409d7c0e281c34
morita-koki <morita82007koki@gmail.com>
2022-07-09 23:20:18 +0900

    ページを統合

--- frontend/src/pages/ingredient/index.page.tsx
+++ frontend/src/pages/ingredient/index.page.tsx
@@ -1,13 +1,17 @@
-import { useRouter } from "next/router";
+import { useState } from "react";
 
 /* original component */
 import { Layout } from "src/components/Layout";
+import { CocktailCards } from "src/components/cocktailCard";
 import { IngredientCard } from "src/components/IngredientCard";
+import { IngredientShowField } from "src/components/IngredientShowField";
 
 /* type */
 import type { Ingredient } from "src/utils/types/type";
 
 /* MUI Component */
+import { Link } from "@mui/material";
+import { Typography } from "@mui/material";
 
 const IngredientPage: React.VFC = (): JSX.Element => {
   const sample_ingredients: Ingredient[] = [
@@ -27,23 +31,59 @@ const IngredientPage: React.VFC = (): JSX.Element => {
       content: "whisky content",
     },
   ];
+  const sample_cocktails: any[] = [];
 
-  const router = useRouter();
+  const [showIngredientDetails, setShowIngredientDetails] =
+    useState<boolean>(false);
 
   return (
     <Layout>
-      {sample_ingredients?.map((ingredient) => {
-        return (
-          <IngredientCard //TODO: IngredientCardの修正
-            canDelete={false}
-            imgSrc={ingredient.img}
-            name={ingredient.name}
+      {!showIngredientDetails ? (
+        sample_ingredients?.map((ingredient) => {
+          return (
+            <IngredientCard //TODO: IngredientCardの修正
+              canDelete={false}
+              imgSrc={ingredient.img}
+              name={ingredient.name}
+              onClick={() => {
+                console.log("clicked");
+                setShowIngredientDetails(true);
+              }}
+            />
+          );
+        })
+      ) : (
+        <>
+          <Link
+            component="button"
+            variant="body2"
             onClick={() => {
-              router.push("/ingredient/show");
+              console.log("clicked");
+              setShowIngredientDetails(false);
             }}
-          />
-        );
-      })}
+            sx={{ mx: 2 }}
+          >
+            材料一覧ページへ戻る
+          </Link>
+          <IngredientShowField ingredient={sample_ingredients[0]} />
+          <Typography
+            component="div"
+            variant="subtitle1"
+            sx={{ fontWeight: "bold", mx: 2 }}
+          >
+            作れるカクテル
+          </Typography>
+          {/* //TODO: CocktailCardに変更する */}
+          <CocktailCards cocktails={sample_cocktails.slice(0, 3)} />
+          <Typography
+            component="div"
+            variant="subtitle1"
+            sx={{ fontWeight: "bold", mx: 2, textAlign: "right" }}
+          >
+            もっと見る
+          </Typography>
+        </>
+      )}
     </Layout>
   );
 };
