43d7fd9d02212da1d30315febf662c22a0b82499
YAMADA Riko <lycopene109@outlook.jp>
2022-07-09 09:47:41 +0900

    FIX: コード整形

--- frontend/src/components/cocktailCard.tsx
+++ frontend/src/components/cocktailCard.tsx
@@ -1,8 +1,8 @@
-import { Card, Grid } from '@mui/material';
-import Link from 'next/link';
-import type { VFC } from 'react';
+import { Card, Grid } from "@mui/material";
+import Link from "next/link";
+import type { VFC } from "react";
 
-import {CocktailImg }from './CocktailImg';
+import { CocktailImg } from "./CocktailImg";
 
 type RecipeProps = {
   id: any;
@@ -12,13 +12,12 @@ type RecipeProps = {
   drinkMethod: any;
   glassType: any;
   ingredients: any;
-}
+};
 
 type CocktailProps = {
   cocktails: any[];
   recipe: RecipeProps;
 };
-
 
 export const CocktailCard: VFC<CocktailProps> = ({ cocktails }) => {
   return (
@@ -26,20 +25,29 @@ export const CocktailCard: VFC<CocktailProps> = ({ cocktails }) => {
       {cocktails.map((cocktail, recipe) => {
         return (
           <Link href={`/cocktail-recipe/${cocktail.id}`} key={cocktail.id}>
-            <Card style={{padding:"0.5rem", margin:"0.25rem"}}>
-            <Grid container spacing={3} sx={{display:"flex", alignItems:"center"}}>
-              <Grid item xs={3}>
-                <CocktailImg recipe={recipe} width={50} height={50} />
+            <Card style={{ padding: "0.5rem", margin: "0.25rem" }}>
+              <Grid
+                container
+                spacing={3}
+                sx={{ display: "flex", alignItems: "center" }}>
+                <Grid item xs={3}>
+                  <CocktailImg recipe={recipe} width={50} height={50} />
+                </Grid>
+                <Grid item xs={9}>
+                  <h1 style={{ padding: "0.5em 0", fontWeight: "bold" }}>
+                    {cocktail.name}
+                  </h1>
+                  <h2
+                    style={{
+                      fontSize: "0.75rem",
+                      lineHeight: "1rem",
+                      color: "#2196f3",
+                    }}>
+                    {"すぐ作れる！"}
+                  </h2>
+                </Grid>
               </Grid>
-              <Grid item xs={9}>
-                <h1 style={{padding: "0.5em 0", fontWeight:"bold"}}>{cocktail.name}</h1>
-                <h2 style={{fontSize: "0.75rem", lineHeight: "1rem", color:'#2196f3'}}>
-                  {'すぐ作れる！'}
-                </h2>
-              </Grid>
-            </Grid>
             </Card>
-           
           </Link>
         );
       })}
--- frontend/src/pages/cocktails/index.page.tsx
+++ frontend/src/pages/cocktails/index.page.tsx
@@ -1,12 +1,12 @@
 //mochikun用
-import { useRouter } from 'next/router';
-import type { VFC } from 'react';
-import { useEffect } from 'react';
-import { CocktailCard } from 'src/components/CocktailCard';
-import { Layout } from 'src/components/Layout';
-import { ToRegisterModal } from 'src/components/ToRegisterModal';
-import { useGetCocktails } from 'src/utils/hooks/useGetCocktails';
-import { useGetRecipe } from 'src/utils/hooks/useGetRecipe';
+import { useRouter } from "next/router";
+import type { VFC } from "react";
+import { useEffect } from "react";
+import { CocktailCard } from "src/components/CocktailCard";
+import { Layout } from "src/components/Layout";
+import { ToRegisterModal } from "src/components/ToRegisterModal";
+import { useGetCocktails } from "src/utils/hooks/useGetCocktails";
+import { useGetRecipe } from "src/utils/hooks/useGetRecipe";
 
 const CocktailPage: VFC = () => {
   const { cocktails, loading, error, getCocktailsFn } = useGetCocktails();
@@ -21,12 +21,12 @@ const CocktailPage: VFC = () => {
 
   return (
     <Layout>
-      {loading && <p>ローディング中です</p>}
-      {cocktails?.length == 0 && <ToRegisterModal name="作成可能なカクテル" />}
+      {loading && <p style={{ margin: "1rem" }}>ローディング中です</p>}
+      {cocktails?.length == 0 && <ToRegisterModal name='作成可能なカクテル' />}
       {error && <p>エラーが発生しました</p>}
       {cocktails && (
-        <div className="container">
-          <CocktailCard cocktails={cocktails} recipe={recipe}/>
+        <div className='container'>
+          <CocktailCard cocktails={cocktails} recipe={recipe} />
         </div>
       )}
     </Layout>
