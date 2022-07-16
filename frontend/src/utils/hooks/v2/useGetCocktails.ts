ac11ee03959ed180ca332f6d4f5bffc5323bd42c
Hide-nu <nkret805worm@gmail.com>
2022-07-16 13:28:13 +0900

    レスポンスの型追加

--- frontend/src/utils/hooks/v2/useGetCocktails.ts
+++ frontend/src/utils/hooks/v2/useGetCocktails.ts
@@ -3,9 +3,21 @@ import { useGetApi } from "src/utils/hooks/useApi";
 
 import { Context } from "../../contexts/provider";
 
+type CocktailsResponseType = {
+  cocktails: {
+    id: number;
+    name: string;
+    strength: number;
+    drinkMethodId: number;
+    glassTypeId: number;
+    cookExplanation: string;
+    createdAt: string;
+    updatedAt: string;
+  }[];
+};
 export const useGetCocktails = () => {
   const { uuid } = useContext(Context);
-  const { loading, error, response, getFn } = useGetApi(
+  const { loading, error, response, getFn } = useGetApi<CocktailsResponseType>(
     `/v2/queries/get_cocktails?uuid=${uuid}`
   );
 
@@ -14,13 +26,6 @@ export const useGetCocktails = () => {
     getFn();
   }, [uuid, getFn]);
 
-  const cocktails = response?.cocktails?.map((cocktail: any) => {
-    return {
-      id: cocktail?.id,
-      name: cocktail?.name,
-      strength: cocktail?.strength,
-      explanation: cocktail?.cookExplanation,
-    };
-  });
+  const cocktails = response?.cocktails;
   return { cocktails, loading, error, getCocktailsFn };
 };
